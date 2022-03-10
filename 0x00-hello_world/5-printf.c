a3e239118da74ca710e1b801a3b7f126f28e3ba9
wondm <wondez56@gmai.com>
2022-03-10 05:01:43 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description - write the given statement
+*Return - 0 if it is true
+*/
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art, \n");
+    return (0);
+}
